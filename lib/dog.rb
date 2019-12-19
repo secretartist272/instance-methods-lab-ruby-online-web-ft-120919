7df224345f04a5911873cb005cdd2eea8b5bd791
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 02:53:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+
+
+
+
+
+Dog.new 
+
+fido = Dog.new
+
+fido.bark
+
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+Person.new 
+
+ada = Person.new
+
+ada.talk
+
+ada.walk
